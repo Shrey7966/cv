c155f7ae06aef280046462adbff532b76ca5f660
Shrey7966 <94635069+Shrey7966@users.noreply.github.com>
2022-08-16 05:14:53 -0400

    Initial CV-demo

---
+++ app.js
@@ -0,0 +1,10 @@
+// function displayTime() {
+//     time = new Date();
+//     document.getElementById('days').innerHTML = time;
+// }
+// setInterval(displayTime, 1000);
+
+// const now = new Date();
+// console.log(now)
+
+
